a3130fe3ac7edfc76b8318dd87812fb80884f71f
PieterBu <pieter.bunnenberg@tum.de>
2015-12-11 12:07:39 +0100

    send_goals

--- simple_navigation_goals/src/send_goals.cpp
+++ simple_navigation_goals/src/send_goals.cpp
@@ -2,25 +2,43 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Pose.h"
 #include "goal_subscriber_class.hpp"
+#include <string>
+#include <sstream>
 
 int main(int argc, char** argv){
 
 ros::init(argc, argv, "send_goals");
 ros::NodeHandle n;
 
-
+float x_pos;
+geometry_msgs::Pose goal_msg;
 
 ros::Publisher goal= n.advertise<geometry_msgs::Pose>("my_goals/goal",100);
 
-ros::Rate r(10);
+ros::Rate r(1);
 
+if(argc == 2){
+ROS_INFO("%s", *(argv+1));
+std::stringstream sstr(*(argv+1));
+sstr >> x_pos;
 
+ROS_INFO("bla  bala %f", x_pos);
+goal_msg.position.x= x_pos;
+goal_msg.position.y = 19.75;
+goal_msg.position.z = 0.000;
+goal_msg.orientation.x = 0.000;
+goal_msg.orientation.y = 0.000;
+goal_msg.orientation.z = 1;
+goal_msg.orientation.w = 0;
+goal.publish(goal_msg);
+r.sleep();
+return 0;
+}
 
-geometry_msgs::Pose goal_msg;
 
 
 //insert your waypoints here
-int i=25;
+int i=1;
 while(i>0){
     goal_msg.position.x= i;
     goal_msg.position.y = 19.75;
